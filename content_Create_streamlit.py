fe2e0214ed76665732d0b4333fcd33d7e6a4e400
donaldjnnt <47189574+donaldjnnt@users.noreply.github.com>
2021-06-23 00:58:05 +0530

    Update content_Create_streamlit.py

--- content_Create_streamlit.py
+++ content_Create_streamlit.py
@@ -224,7 +224,7 @@ def Extract_urls(Topic):
 
 def Extract_URLs_New(Topic):
     query = Topic
-    driver_location = "https://github.com/donaldjnnt/content_create1/blob/main/chromedriver.exe"
+    driver_location = "C:\Users\Darcey\Documents\NEW_PYTHON\chromedriver.exe"
     options = webdriver.ChromeOptions()
     options.add_argument('--lang=en,en_US')
     # options.add_argument('--disable-gpu')
