16f6fb25ad954b05f45aae846ee339b4b691841e
donaldjnnt <47189574+donaldjnnt@users.noreply.github.com>
2021-06-23 01:26:38 +0530

    Update content_Create_streamlit.py

--- content_Create_streamlit.py
+++ content_Create_streamlit.py
@@ -243,7 +243,7 @@ def Extract_URLs_New(Topic):
     options.add_argument('--no-sandbox')
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(), options=options)
 #    driver = webdriver.Chrome(executable_path=driver_location, options=options)
 
     driver.get("https://www.google.com/search?q={}&oq={}&hl=en&num=10".format(urllib.parse.quote(query),urllib.parse.quote(query)))
